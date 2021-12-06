0a6ebde3608dc9360e1e2aab76208ec2d2012fda
Bas Verhoeven <b.verhoeven@homewizard.nl>
2021-12-06 10:24:58 +0000

    Add reducer helper functions

---
+++ shared/reducers.ts
@@ -0,0 +1,3 @@
+export const sum = (prev: number, current: number) => prev + current
+export const max = (prev: number, current: number) => Math.max(current, prev)
+export const min = (prev: number, current: number) => Math.min(current, prev)
