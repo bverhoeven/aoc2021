ec0a6f57737cb912feb5f908a83922f3d4be9241
Bas Verhoeven <b.verhoeven@homewizard.nl>
2021-12-10 20:34:18 +0000

    Add 'multiply' reducer

--- shared/reducers.ts
+++ shared/reducers.ts
@@ -1,3 +1,4 @@
 export const sum = (prev: number, current: number) => prev + current
 export const max = (prev: number, current: number) => Math.max(current, prev)
 export const min = (prev: number, current: number) => Math.min(current, prev)
+export const multiply = (prev: number, current: number) => prev * current
